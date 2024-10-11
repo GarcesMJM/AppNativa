b10d0f8175637fc1e23c8d0c7a8f9c6f5320d68a
JuanJClR <juanj.calderon@upb.edu.co>
2024-10-11 03:31:55 -0500

    Modificado diseño del home

--- app/src/main/java/com/example/appnativa/MainActivity.kt
+++ app/src/main/java/com/example/appnativa/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.app_nativa
+package com.example.appnativa
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,30 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -55,19 +39,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier){
+fun HomeScreen(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
     ) {
-        Row (
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(5.dp)
                 .background(Color(0xFF89E4C4)),
             horizontalArrangement = Arrangement.End
-        ){
+        ) {
             Button(
                 modifier = Modifier,
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF89E4C4)),
@@ -80,18 +64,16 @@ fun HomeScreen(modifier: Modifier = Modifier){
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(15.dp),
                     textAlign = TextAlign.Center
-
-                )}
-
+                )
+            }
         }
         Column(
             modifier = Modifier
-                .fillMaxSize(1f)
-                .padding(20.dp, ),
+                .fillMaxSize()
+                .padding(20.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-
             Text(
                 text = "Bienvenido a Nombre_app",
                 color = Color.White,
@@ -99,34 +81,30 @@ fun HomeScreen(modifier: Modifier = Modifier){
                 fontWeight = FontWeight.Medium,
                 modifier = Modifier.padding(vertical = 16.dp)
             )
-
             Button(
                 onClick = { /* TODO: Handle login */ },
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxWidth(0.8f)
                     .padding(vertical = 8.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF89E4C4)),
                 shape = RoundedCornerShape(8.dp)
             ) {
                 Text("Mis listas", color = Color.Black)
             }
-
             Button(
                 onClick = { /* TODO: Handle registration */ },
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxWidth(0.8f)
                     .padding(vertical = 8.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF89E4C4)),
                 shape = RoundedCornerShape(8.dp)
             ) {
                 Text("Crear lista", color = Color.Black)
             }
-
             Spacer(modifier = Modifier.height(32.dp))
         }
     }
 }
-
 
 @Preview(showBackground = true, showSystemUi = true, name = "home")
 @Composable
