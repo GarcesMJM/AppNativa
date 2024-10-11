444ff620f7806c85a20224bea48099353e3415e8
GarcesMJM <89167308+GarcesMJM@users.noreply.github.com>
2024-10-11 00:31:19 -0500

    Agregada la vista del home

---
+++ app/src/main/java/com/example/appnativa/MainActivity.kt
@@ -0,0 +1,137 @@
+package com.example.app_nativa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appnativa.ui.theme.AppNativaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppNativaTheme {
+                HomeScreen(modifier = Modifier)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)
+                .background(Color(0xFF89E4C4)),
+            horizontalArrangement = Arrangement.End
+        ){
+            Button(
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF89E4C4)),
+                onClick = {}
+            ) {
+                Text(
+                    text = "Iniciar Sesión",
+                    color = Color.Black,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(15.dp),
+                    textAlign = TextAlign.Center
+
+                )}
+
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize(1f)
+                .padding(20.dp, ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Bienvenido a Nombre_app",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+
+            Button(
+                onClick = { /* TODO: Handle login */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF89E4C4)),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text("Mis listas", color = Color.Black)
+            }
+
+            Button(
+                onClick = { /* TODO: Handle registration */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF89E4C4)),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text("Crear lista", color = Color.Black)
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true, name = "home")
+@Composable
+fun HomePreview() {
+    AppNativaTheme {
+        HomeScreen()
+    }
+}
